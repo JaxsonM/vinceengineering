9ce3a881f366f8895800acc37b28fb44053c2db7
Jaxson Madison <a02336244@aggies.usu.edu>
2024-05-31 19:17:27 -0600

    Added resizing for better viewing on mobile screens

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
-import { FaCaretDown, FaCaretUp } from 'react-icons/fa';
+import { FaCaretDown, FaCaretUp, FaBars, FaTimes } from 'react-icons/fa';
 
 const NavBar: React.FC = () => {
   const [isServicesDropdownOpen, setIsServicesDropdownOpen] = useState(false);
   const [isServiceClicked, setIsServiceClicked] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const dropdownRef = useRef<HTMLUListElement>(null);
   const location = useLocation();
 
@@ -22,6 +23,7 @@ const NavBar: React.FC = () => {
   const handleServiceClick = () => {
     setIsServiceClicked(true);
     setIsServicesDropdownOpen(false);
+    setIsMobileMenuOpen(false); // Close mobile menu on link click
   };
 
   // Effect to set the state of the services button based on the current path
@@ -34,107 +36,85 @@ const NavBar: React.FC = () => {
   // Determine if any service page is active
   const isServicesActive = location.pathname.startsWith('/services');
 
+  // Function to toggle the mobile menu
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+    if (isMobileMenuOpen) {
+      setIsServicesDropdownOpen(false);
+    }
+  };
+
   return (
-    <nav className="bg-[#343a40] text-white p-4 flex justify-between items-center h-20 px-10 font-roboto fixed top-0 w-full z-50">
+    <nav className="bg-[#343a40] text-white p-5 flex justify-between items-center shadow fixed top-0 w-full z-50">
       {/* Company Name and Slogan */}
-      <div className="text-center">
-        <div className="text-[#A7EA53] text-sm">
+      <div className="flex text-center justify-between items-center w-full md:w-auto">
+        <div className="flex-col">
+        <div className="text-[#A7EA53] text-xs md:text-sm">
           POWER. CONTROLS. COMMUNICATIONS.
         </div>
-        <div className="text-3xl font-bold text-[#B4DCFA]">
+        <div className="text-2xl md:text-3xl font-bold text-[#B4DCFA] mx-4">
           <NavLink to="/" end>
             VINC<span className="underline">EE</span>NGIN<span className="underline">EE</span>RING
           </NavLink>
         </div>
+        </div>
+        <button onClick={toggleMobileMenu} className="text-3xl md:hidden focus:outline-none">
+          {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+        </button>
       </div>
 
       {/* Navigation Links */}
-      <ul className="flex space-x-10 items-center text-2xl">
-        <li>
-          <NavLink to="/" className={({ isActive }) => isActive ? "text-[#A7EA53] hover:text-[#6B9635]" : "hover:text-gray-300"} end>Home</NavLink>
+      <ul className={`md:flex md:text-2xl md:items-center md:space-x-10 md:relative absolute bg-[#343a40] w-full md:w-auto md:py-0 py-4 md:pl-0 pl-7 transition-all duration-500 ease-in ${isMobileMenuOpen ? 'top-20 opacity-100' : 'top-[-400px] opacity-0 md:opacity-100 md:top-0'}`}>
+        <li className="mx-4 my-6 md:my-0">
+          <NavLink to="/" className={({ isActive }) => isActive ? "text-[#A7EA53] hover:text-[#6B9635]" : "hover:text-gray-300"} end onClick={() => setIsMobileMenuOpen(false)}>Home</NavLink>
         </li>
-        <li>
-          <NavLink to="/about" className={({ isActive }) => isActive ? "text-[#A7EA53] hover:text-[#6B9635]" : "hover:text-gray-300"}>About Us</NavLink>
+        <li className="mx-4 my-6 md:my-0">
+          <NavLink to="/about" className={({ isActive }) => isActive ? "text-[#A7EA53] hover:text-[#6B9635]" : "hover:text-gray-300"} onClick={() => setIsMobileMenuOpen(false)}>About Us</NavLink>
         </li>
 
         {/* Services Dropdown */}
-        <li
-          className="relative"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
-          <div
-            className={`hover:text-gray-300 focus:outline-none flex items-center ${isServicesActive || isServiceClicked ? 'text-[#A7EA53]' : ''}`}
-          >
+        <li className="relative mx-4 my-6 md:my-0" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <div className={`hover:text-gray-300 focus:outline-none flex items-center ${isServicesActive || isServiceClicked ? 'text-[#A7EA53]' : ''}`} onClick={() => setIsServicesDropdownOpen(!isServicesDropdownOpen)}>
             Services {isServicesDropdownOpen ? <FaCaretUp className="ml-1" /> : <FaCaretDown className="ml-1" />}
           </div>
-          <ul
-            ref={dropdownRef}
-            className={`absolute left-0 mt-2 w-48 bg-white text-black rounded-md shadow-lg transform transition-all duration-300 z-50 ${
-              isServicesDropdownOpen ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'
-            } overflow-hidden`}
-          >
+          <ul ref={dropdownRef} className={`absolute left-0 mt-2 w-48 bg-white text-black rounded-md shadow-lg transform transition-all duration-300 z-50 ${isServicesDropdownOpen ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'} overflow-hidden`}>
             <li>
-              <NavLink
-                to="/services/power-systems"
-                className={({ isActive }) => isActive ? "block px-4 py-2 bg-gray-200" : "block px-4 py-2 hover:bg-gray-200"}
-                onClick={handleServiceClick}
-              >
+              <NavLink to="/services/power-systems" className={({ isActive }) => isActive ? "block px-4 py-2 bg-gray-200" : "block px-4 py-2 hover:bg-gray-200"} onClick={handleServiceClick}>
                 Power Systems
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/services/control-systems"
-                className={({ isActive }) => isActive ? "block px-4 py-2 bg-gray-200" : "block px-4 py-2 hover:bg-gray-200"}
-                onClick={handleServiceClick}
-              >
+              <NavLink to="/services/control-systems" className={({ isActive }) => isActive ? "block px-4 py-2 bg-gray-200" : "block px-4 py-2 hover:bg-gray-200"} onClick={handleServiceClick}>
                 Control Systems
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/services/network-communications"
-                className={({ isActive }) => isActive ? "block px-4 py-2 bg-gray-200" : "block px-4 py-2 hover:bg-gray-200"}
-                onClick={handleServiceClick}
-              >
+              <NavLink to="/services/network-communications" className={({ isActive }) => isActive ? "block px-4 py-2 bg-gray-200" : "block px-4 py-2 hover:bg-gray-200"} onClick={handleServiceClick}>
                 Network & Communications
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/services/instrumentation"
-                className={({ isActive }) => isActive ? "block px-4 py-2 bg-gray-200" : "block px-4 py-2 hover:bg-gray-200"}
-                onClick={handleServiceClick}
-              >
+              <NavLink to="/services/instrumentation" className={({ isActive }) => isActive ? "block px-4 py-2 bg-gray-200" : "block px-4 py-2 hover:bg-gray-200"} onClick={handleServiceClick}>
                 Instrumentation
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/services/data-capture"
-                className={({ isActive }) => isActive ? "block px-4 py-2 bg-gray-200" : "block px-4 py-2 hover:bg-gray-200"}
-                onClick={handleServiceClick}
-              >
+              <NavLink to="/services/data-capture" className={({ isActive }) => isActive ? "block px-4 py-2 bg-gray-200" : "block px-4 py-2 hover:bg-gray-200"} onClick={handleServiceClick}>
                 Data Capture
               </NavLink>
             </li>
             <li>
-              <NavLink
-                to="/services/support"
-                className={({ isActive }) => isActive ? "block px-4 py-2 bg-gray-200" : "block px-4 py-2 hover:bg-gray-400"}
-                onClick={handleServiceClick}
-              >
+              <NavLink to="/services/support" className={({ isActive }) => isActive ? "block px-4 py-2 bg-gray-400" : "block px-4 py-2 hover:bg-gray-400"} onClick={handleServiceClick}>
                 Support
               </NavLink>
             </li>
           </ul>
         </li>
-        <li>
-          <NavLink to="/projects" className={({ isActive }) => isActive ? "text-[#A7EA53] hover:text-[#6B9635]" : "hover:text-gray-300"}>Projects</NavLink>
+        <li className="mx-4 my-6 md:my-0">
+          <NavLink to="/projects" className={({ isActive }) => isActive ? "text-[#A7EA53] hover:text-[#6B9635]" : "hover:text-gray-300"} onClick={() => setIsMobileMenuOpen(false)}>Projects</NavLink>
         </li>
-        <li>
-          <NavLink to="/contact" className={({ isActive }) => isActive ? "text-[#A7EA53] hover:text-[#6B9635]" : "hover:text-gray-300"}>Contact Us</NavLink>
+        <li className="mx-4 my-6 md:my-0">
+          <NavLink to="/contact" className={({ isActive }) => isActive ? "text-[#A7EA53] hover:text-[#6B9635]" : "hover:text-gray-300"} onClick={() => setIsMobileMenuOpen(false)}>Contact Us</NavLink>
         </li>
       </ul>
     </nav>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,6 @@ import Slideshow from '../components/Slideshow';
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col items-center pt-20">
-        {/* <h1 className="text-4xl font-bold bg-slate-50">Welcome to VinceEngineering</h1>
-        <p className="mt-4 text-lg">Your partner in POWER, CONTROLS, and COMMUNICATIONS.</p> */}
       <div className="w-full bg-cover bg-center" style={{ backgroundImage: "url('/images/imageBackground.jpg')" }}>
         <Slideshow />
       </div>
@@ -15,15 +13,15 @@ const Home: React.FC = () => {
           Contact Us
         </button>
       </div>
-      <div className="flex bg-[#D8D8D8] w-full h-[calc(70vh-10rem)]">
-        <div className="w-1/2 p-10">
-          <h1 className="text-6xl">What We Do</h1>
+      <div className="flex flex-col md:flex-row bg-[#D8D8D8] w-full h-auto md:h-[calc(70vh-10rem)]">
+        <div className="w-full md:w-1/2 p-6 md:p-10">
+          <h1 className="text-3xl md:text-6xl">What We Do</h1>
           <div className="border-b-4 border-[#1D6FC6] w-full my-4"></div>
-          <p className="text-2xl">
+          <p className="text-lg md:text-2xl">
             At VinceEngineering, we specialize in delivering comprehensive electrical engineering solutions. Our expertise spans across power systems, control systems, network and communications, instrumentation, and data capture. With a strong background in service and support, we are committed to ensuring optimal performance and reliability for our clients. Located in Salt Lake City, UT, our team is dedicated to providing innovative solutions that drive efficiency and success in various industries.
           </p>
         </div>
-        <div className="w-1/2 bg-cover bg-center" style={{ backgroundImage: "url('/images/Picture4.jpg')" }}></div>
+        <div className="w-full md:w-1/2 bg-cover bg-center h-64 md:h-auto" style={{ backgroundImage: "url('/images/Picture4.jpg')" }}></div>
       </div>
     </div>
   );
